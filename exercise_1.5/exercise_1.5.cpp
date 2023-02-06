f612de09f8fda36e311f99e5a7d9546707632aaf
xtxyTge2 <f1AZnn@protonmail.com>
2023-02-06 12:57:11 +0100

    Changed dir structure. Completed exercise 1.5

---
+++ exercise_1.5/exercise_1.5.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+static float fahrenheit_to_celsius(int fahrenheit_value);
+
+int main()
+{
+	int min_fahrenheit = 0;
+	int max_fahrenheit = 300;
+	int step_size = 20;
+
+	int steps = (max_fahrenheit - min_fahrenheit)/ step_size;
+	printf("Fahrenheit\tCelcius\n");
+	for (int i = steps; i >= 0; i--) {
+		int fahrenheit_value = i * step_size;
+		float celcius_value = fahrenheit_to_celsius(fahrenheit_value);
+
+		printf("%10d\t%7.1f\n", fahrenheit_value, celcius_value);
+	}
+}
+
+static float fahrenheit_to_celsius(int fahrenheit_value) {
+	return (5.0f / 9.0f) * (((float)fahrenheit_value) - 32.0f);
+}
